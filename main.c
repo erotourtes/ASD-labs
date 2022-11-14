26419176464993ff235dece0433731324a26e25a
sirmax <erotourtes@gmail.com>
2022-11-14 16:35:42 +0200

    initial

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+// #include <ncurses.h>
+
+int main (int argc, char *argv[])
+{
+  // initscr();
+  // mvadch();
+  // refresh(); napms(500);
+  // getch();
+  // endwin();
+  printf("Hello from 1.6");
+
+  return 0;
+}
