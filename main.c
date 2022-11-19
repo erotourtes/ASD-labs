978dc1968a2a6b45bdb23f15f31aad77ea9440be
sirmax <erotourtes@gmail.com>
2022-11-19 12:51:03 +0200

    circle

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include <ncurses.h>
 
+void print(short i, short j) {
+      mvprintw(i, j, "%i", 1);
+
+      refresh();
+      napms(350);
+}
+
 int main (int argc, char *argv[])
 {
   initscr();
@@ -9,17 +16,36 @@ int main (int argc, char *argv[])
   napms(1000);
   clear();
 
-  int rows = 25; 
-  int colums = 80;
-  
-  for (short i = 0; i < rows; i++) {
-    for (short j = 0; j < colums; j++) {
-      mvprintw(i, j, "%i", 1);
+  // int rows = 25; 
+  // int colums = 80;
 
-      refresh();
-      napms(3);
+  int rows = 6; 
+  int colums = 6;
+
+  for (short p = 0; p < 1; p++) {
+
+    //to right
+    for (short j = 0; j < colums; j++) {
+      print(0, j);
     }
+
+    //to down
+    for (short i = 1; i < rows; i++) {
+      print(i, colums - 1);
+    }
+
+    //to left
+    for (short j = colums - 1 - 1; j > 0; j--) {
+      print(rows - 1, j);
+    }
+
+    //to up
+    for (short i = rows - 1; i > 0; i--) {
+      print(i, 0);
+    }
+
   }
+  
 
 	getch();
 	endwin();	
