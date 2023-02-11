955c0a10afb72c310b78d3f45f707ba09ca7637b
Max Sir <erotourtes@gmail.com>
2023-02-11 15:42:28 +0200

    compare.c

--- 2-2.1/utils/compare.c
+++ 2-2.1/utils/compare.c
@@ -4,8 +4,20 @@
 #include "../recursionDepth.c"
 #include "../recursionHeight.c"
 #include "../recursionMixed.c"
+#include "../loop.c"
 
+#include "./types.c"
 
-void compare() { 
-    asinh(0);
+void compare() {
+  u32 n = 5;
+
+  printf("Calculator\t\tDepth\t\t\tHeight\t\t\tMixed\t\t\tLoop\n");
+  for (double x = 0; x < 1; x += 0.01) {
+    printf("%.15lf\t", asinh(x));
+    printf("%.15lf\t", recursionDepth(x, n));
+    printf("%.15lf\t", recursionHeight(x, n));
+    printf("%.15lf\t", recursionMixed(x, n));
+    printf("%.15lf\t", loop(x, n));
+    printf("\n");
+  }
 }
