e1ed1c7b2e13dc2055a528cc9af07d7b36c729c7
Max Sir <erotourtes@gmail.com>
2023-02-21 09:34:23 +0200

    added Report

--- 2-2.2/report/List.c
+++ 2-2.2/report/List.c
@@ -1,6 +1,17 @@
-#include "./utils/print_color.c"
 #include <stdio.h>
 #include <stdlib.h>
+
+void set_urgent_color() {
+    printf("\033[41;30m");
+}
+
+void set_info_color() {
+    printf("\033[44;30m");
+}
+
+void reset_color() {
+    printf("\033[0m");
+}
 
 typedef struct Node {
   unsigned int value;
