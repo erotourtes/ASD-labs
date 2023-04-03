d360afe9bf3b689392323fe2de8dfedb9b3fd6c0
Max Sir <erotourtes@gmail.com>
2023-04-03 15:26:33 +0300

    test commit

--- 2-2.3/draw_graph.c
+++ 2-2.3/draw_graph.c
@@ -27,20 +27,21 @@ int in_range(int x, int a, int b) {
     return a < x && x < b || b < x && x < a;
 }
 
-int through_center(Point* p, int i, int j, int n) {
+int through_center(Point *p, int i, int j, int n) {
     Point p1 = p[i];
     Point p2 = p[j];
-    Point center = p[n-1];
+    Point center = p[n - 1];
     return (p1.x + p2.x) / 2 == center.x && (p1.y + p2.y) / 2 == center.y;
 }
+
 int is_overlapping(Point *points, int n, int i, int j) {
     Point p1 = points[i];
     Point p2 = points[j];
 
     for (int k = 0; k < n; k++) {
         if (k == i || k == j) continue;
-        Point cur = points[k];
 
+        Point cur = points[k];
         int is_in_row = p1.y == p2.y && p1.y == cur.y && in_range(cur.x, p1.x, p2.x);
         int is_in_column = p1.x == p2.x && p1.x == cur.x && in_range(cur.y, p1.y, p2.y);
         if (is_in_row || is_in_column)
@@ -50,7 +51,7 @@ int is_overlapping(Point *points, int n, int i, int j) {
     return 0;
 }
 
-int is_directions_overlaps(Matrix matrix, int i, int j) {
+int is_arrows_overlaps(Matrix matrix, int i, int j) {
     int **m = matrix.val;
 
     return m[i][j] && m[j][i];
@@ -98,6 +99,73 @@ void draw_arrow_line(X11 app, Point p1, Point p2, double circle_radius) {
     XDrawLine(app.dis, app.win, app.gc, p1.x, p1.y, p2.x, p2.y);
 }
 
+void draw_through_center(X11 app, Point *points, int i, int j, int circle_radius) {
+    int mid_x = (points[i].x + points[j].x) / 2;
+    int mid_y = (points[i].y + points[j].y) / 2;
+
+    int offsetX = 2.5 * circle_radius;
+    int offsetY = 2.5 * circle_radius;
+
+    double c = sqrt(pow(mid_x - points[i].x, 2) + pow(mid_y - points[i].y, 2));
+    double angle = atan2((double) offsetX, c);
+
+    // flip in opposite direction
+    if (points[i].x > points[j].x && points[i].y > points[j].y) {
+        offsetX = -offsetX;
+        offsetY = -offsetY;
+        // not to overlap with other lines which is dense to each other
+    } else if (points[i].x < points[j].x && points[i].y > points[j].y) {
+        offsetY = -2 * offsetY;
+        offsetX = 1.5 * offsetX;
+    } else if (points[i].x > points[j].x && points[i].y < points[j].y) {
+        offsetY = 2 * offsetY;
+        offsetX = -1.5 * offsetX;
+    }
+
+    Point middle = {mid_x - offsetX * cos(angle), mid_y + offsetY * sin(angle)};
+
+    XDrawLine(app.dis, app.win, app.gc, points[i].x, points[i].y, middle.x, middle.y);
+    draw_arrow_line(app, middle, points[j], circle_radius);
+}
+
+void draw_row_overlap(X11 app, Point *points, int i, int j, int circle_radius) {
+    int mid_x = (points[i].x + points[j].x) / 2;
+
+    int dx = abs(points[i].x - points[j].x);
+    int offset = circle_radius / 2 + log10(dx) * 10;
+
+    if (points[i].x > points[j].x)
+        offset *= -1;
+
+    Point middle = {mid_x, points[i].y + offset};
+
+    XDrawLine(app.dis, app.win, app.gc, points[i].x, points[i].y, middle.x, middle.y);
+    draw_arrow_line(app, middle, points[j], circle_radius);
+}
+
+void draw_column_overlap(X11 app, Point *points, int i, int j, int circle_radius) {
+    int mid_y = (points[i].y + points[j].y) / 2;
+
+    int dy = abs(points[i].y - points[j].y);
+    int offset = circle_radius / 2 + log10(dy) * 10;
+
+    if (points[i].y > points[j].y)
+        offset *= -1;
+
+    Point middle = {points[i].x + offset, mid_y};
+
+    XDrawLine(app.dis, app.win, app.gc, points[i].x, points[i].y, middle.x, middle.y);
+    draw_arrow_line(app, middle, points[j], circle_radius);
+}
+
+void draw_overlapping(X11 app, Point *points, int i, int j, int circle_radius) {
+    if (points[i].y == points[j].y) {
+        draw_row_overlap(app, points, i, j, circle_radius);
+    } else if (points[i].x == points[j].x) {
+        draw_column_overlap(app, points, i, j, circle_radius);
+    }
+}
+
 void draw_graph(X11 app, Matrix matrix) {
     Point *points = get_coordinates(matrix.n, 100);
     int offset = 100;
@@ -112,64 +180,15 @@ void draw_graph(X11 app, Matrix matrix) {
     for (int i = 0; i < matrix.n; i++) {
         for (int j = 0; j < matrix.n; j++) {
             if (matrix.val[i][j] == 0) continue;
+
             if (i == j) {
                 draw_loop(app, circle_radius, (Point) {points[i].x, points[i].y});
-            } else if (1) {
-                if (through_center(points, i, j, matrix.n)) {
-                    int mid_x = (points[i].x + points[j].x) / 2;
-                    int mid_y = (points[i].y + points[j].y) / 2;
-
-                    int offsetX = 2.5 * circle_radius;
-                    int offsetY = 2.5 * circle_radius;
-
-                    double c = sqrt(pow(mid_x - points[i].x, 2) + pow(mid_y - points[i].y, 2));
-                    double angle = atan2((double) offset, c);
-
-                    if (points[i].x < points[j].x && points[i].y < points[j].y ||
-                        points[i].x > points[j].x && points[i].y > points[j].y) {
-                        offsetY *= -1;
-
-                    } else if (points[i].y > points[j].y) {
-                        offsetX *= -1;
-                        offsetY *= -1;
-                    };
-
-                    Point middle = {mid_x - offsetX * cos(angle), mid_y - offsetY * sin(angle)};
-
-                    XDrawLine(app.dis, app.win, app.gc, points[i].x, points[i].y, middle.x, middle.y);
-                    draw_arrow_line(app, middle, points[j], circle_radius);
-                } else if (is_overlapping(points, matrix.n, i, j) || is_directions_overlaps(matrix, i, j)) {
-                    if (points[i].y == points[j].y) {
-                        int mid_x = (points[i].x + points[j].x) / 2;
-
-                        int dx = abs(points[i].x - points[j].x);
-                        int offset = circle_radius / 2 + log10(dx) * 10;
-
-                        if (points[i].x > points[j].x)
-                            offset *= -1;
-
-                        Point middle = {mid_x, points[i].y + offset};
-
-                        XDrawLine(app.dis, app.win, app.gc, points[i].x, points[i].y, middle.x, middle.y);
-                        draw_arrow_line(app, middle, points[j], circle_radius);
-                    } else if (points[i].x == points[j].x) {
-
-                        int mid_y = (points[i].y + points[j].y) / 2;
-
-                        int dy = abs(points[i].y - points[j].y);
-                        int offset = circle_radius / 2 + log10(dy) * 10;
-
-                        if (points[i].y > points[j].y)
-                            offset *= -1;
-
-                        Point middle = {points[i].x + offset, mid_y};
-
-                        XDrawLine(app.dis, app.win, app.gc, points[i].x, points[i].y, middle.x, middle.y);
-                        draw_arrow_line(app, middle, points[j], circle_radius);
-                    }
-                } else {
-                    draw_arrow_line(app, points[i], points[j], circle_radius);
-                }
+            } else if (through_center(points, i, j, matrix.n)) {
+                draw_through_center(app, points, i, j, circle_radius);
+            } else if (is_overlapping(points, matrix.n, i, j) || is_arrows_overlaps(matrix, i, j)) {
+                draw_overlapping(app, points, i, j, circle_radius);
+            } else {
+                draw_arrow_line(app, points[i], points[j], circle_radius);
             }
         }
     }
