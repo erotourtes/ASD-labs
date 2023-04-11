9856ad5fc5bc74d1911f4d4e00626c45e486cc92
Max Sir <erotourtes@gmail.com>
2023-04-11 10:35:30 +0300

    refactor: bettter alghoritm for getting coordinates

--- 2-2.3/graph_coords.c
+++ 2-2.3/graph_coords.c
@@ -1,16 +1,26 @@
-void top_row(int num_in_row, Point *cur_point, Point *points, double distance, int *cur, int rest);
-
-void right_column(int num_in_row, Point *cur_point, Point *points, int distance, int *cur, int rest);
-
-void bottom_row(int num_in_row, Point *cur_point, Point *points, int distance, int *cur, int rest);
-
-void left_column(int num_in_row, Point *cur_point, Point *points, int distance, int *cur, int rest);
-
-void center(int num_in_row, Point *point, int distance, int rest);
-
 void copy_point(Point *from, Point *to) {
     to->x = from->x;
     to->y = from->y;
+}
+
+void compress(int i, int rest, int *distance, int *num_in_row) {
+    if (i == 0) { // top
+        if (rest == 1) { // need to compress top row
+            *distance *= (double) (*num_in_row - 1) / (*num_in_row);
+            (*num_in_row)++;
+        } else if (rest == 2 || rest == 3) {
+            (*num_in_row)++;
+        }
+    }
+
+    if (i == 1 && rest == 3) { // right
+        *distance *= (double) (*num_in_row - 1) / (*num_in_row);
+        (*num_in_row)++;
+    }
+
+    if (i == 2 && (rest == 2 || rest == 3)) { // bottom
+        (*num_in_row)++;
+    }
 }
 
 Point *get_coordinates(int n, int distance) {
@@ -18,68 +28,30 @@ Point *get_coordinates(int n, int distance) {
 
     int k = n - 1; // number of points without center
     int num_in_row = floor((k + 4) / 4);
-    int rest = k - (num_in_row * 4 - 4);
+    int rest = k % 4;
+
+    int directions[][2] = {{1,  0},
+                           {0,  1},
+                           {-1, 0},
+                           {0,  -1}};
+
+    Point cur_point = {0, 0};
     int cur = 0;
+    for (int i = 0; i < 4; i++) {
+        int cur_distance = distance;
+        int cur_num_in_row = num_in_row;
 
-    Point cur_point = {-distance, 0};
+        compress(i, rest, &cur_distance, &cur_num_in_row);
 
-    center(num_in_row, &points[n-1], distance, rest);
-    top_row(num_in_row, &cur_point, points, distance, &cur, rest);
-    right_column(num_in_row, &cur_point, points, distance, &cur, rest);
-    bottom_row(num_in_row, &cur_point, points, distance, &cur, rest);
-    left_column(num_in_row, &cur_point, points, distance, &cur, rest);
+        for (int j = 1; j < cur_num_in_row; j++) {
+            copy_point(&cur_point, &points[cur++]);
+            cur_point.x += directions[i][0] * cur_distance;
+            cur_point.y += directions[i][1] * cur_distance;
+        }
+    }
+
+    points[cur].x = ((num_in_row - 1) * distance + (rest > 1 ? distance : 0)) / 2;
+    points[cur].y = ((num_in_row - 1) * distance) / 2;
 
     return points;
-}
-
-void center(int num_in_row, Point *point, int distance, int rest) {
-    point->x = ((num_in_row - 1) * distance + (rest > 1 ? distance : 0)) / 2;
-    point->y = ((num_in_row - 1) * distance) / 2;
-}
-
-void top_row(int num_in_row, Point *cur_point, Point *points, double distance, int *cur, int rest) {
-    if (rest == 1) { // need to compress top row
-        distance *= (double) (num_in_row - 1) / (num_in_row);
-        cur_point->x = -distance;
-        num_in_row++;
-    } else if (rest == 2 || rest == 3) {
-        num_in_row++;
-    }
-
-    for (int i = 0; i < num_in_row; i++) {
-        cur_point->x += distance;
-        copy_point(cur_point, &points[*cur]);
-        (*cur)++;
-    }
-}
-
-void right_column(int num_in_row, Point *cur_point, Point *points, int distance, int *cur, int rest) {
-    if (rest == 3) {
-        distance *= (double) (num_in_row - 1) / (num_in_row);
-        num_in_row++;
-    }
-    for (int i = 0; i < num_in_row - 1; i++) {
-        cur_point->y += distance;
-        copy_point(cur_point, &points[*cur]);
-        (*cur)++;
-    }
-}
-
-void bottom_row(int num_in_row, Point *cur_point, Point *points, int distance, int *cur, int rest) {
-    if (rest == 2 || rest == 3) {
-        num_in_row++;
-    }
-    for (int i = 0; i < num_in_row - 1; i++) {
-        cur_point->x -= distance;
-        copy_point(cur_point, &points[*cur]);
-        (*cur)++;
-    }
-}
-
-void left_column(int num_in_row, Point *cur_point, Point *points, int distance, int *cur, int rest) {
-    for (int i = 0; i < num_in_row - 2; i++) {
-        cur_point->y -= distance;
-        copy_point(cur_point, &points[*cur]);
-        (*cur)++;
-    }
 }
