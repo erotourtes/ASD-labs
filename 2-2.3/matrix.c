27acf654537a694bed3e2bcea412563f1c1b30a5
Max Sir <erotourtes@gmail.com>
2023-04-19 18:46:47 +0300

    fix: 2-2.3 creating array function

--- 2-2.3/matrix.c
+++ 2-2.3/matrix.c
@@ -1,5 +1,5 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define n1 2
 #define n2 1
@@ -7,65 +7,72 @@
 #define n4 9
 
 typedef struct {
-    int **val;
-    int n;
+  int **val;
+  int n;
 } Matrix;
 
-int **rand_matrix(int n);
+double **rand_matrix(int n);
 
-int **mulmr(double k, int **matrix, int n);
+int **mulmr(double k, double **matrix, int n);
 
 void free_matrix(Matrix matrix);
 
 void print_matrix(Matrix matrix, int is_directed);
 
 Matrix get_boolean_matrix() {
-    srand(n1 * 1000 + n2 * 100 + n3 * 10 + n4);
+  srand(n1 * 1000 + n2 * 100 + n3 * 10 + n4);
 
-    int n = 10 + n3;
-    int **matrix = rand_matrix(n);
-    mulmr(1.0 - n3 * 0.02 - n4 * 0.005 - 0.25, matrix, n);
+  int n = 10 + n3;
+  double **matrix = rand_matrix(n);
+  int ** boolean_matrix = mulmr(1.0 - n3 * 0.02 - n4 * 0.005 - 0.25, matrix, n);
 
-    return (Matrix){matrix, n};
+  for (int i = 0; i < n; i++)
+    free(matrix[i]);
+  free(matrix);
+
+  return (Matrix){boolean_matrix, n};
 }
 
-int **rand_matrix(int n) {
-    int **matrix = (int **) malloc(n * sizeof(int *));
-    for (int i = 0; i < n; i++) {
-        matrix[i] = (int *) malloc(n * sizeof(int));
-        for (int j = 0; j < n; j++) {
-            matrix[i][j] = rand() % 3;
-        }
+double **rand_matrix(int n) {
+  double **matrix = (double **)malloc(n * sizeof(double *));
+  for (int i = 0; i < n; i++) {
+    matrix[i] = (double *)malloc(n * sizeof(double));
+    for (int j = 0; j < n; j++) {
+      matrix[i][j] = 2.0 * rand() / RAND_MAX;
     }
+  }
 
-    return matrix;
+  return matrix;
 }
 
-int **mulmr(double k, int **matrix, int n) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            if (matrix[i][j] * k > 1) matrix[i][j] = 1;
-            else matrix[i][j] = 0;
-        }
-    }
+int **mulmr(double k, double **matrix, int n) {
+  int **result = (int **)malloc(n * sizeof(int *));
+  for (int i = 0; i < n; i++)
+    result[i] = (int *)malloc(n * sizeof(int));
+
+  for (int i = 0; i < n; i++)
+    for (int j = 0; j < n; j++)
+        result[i][j] = matrix[i][j] * k;
+
+  return result;
 }
 
 void free_matrix(Matrix matrix) {
-    for (int i = 0; i < matrix.n; i++)
-        free(matrix.val[i]);
-    free(matrix.val);
+  for (int i = 0; i < matrix.n; i++)
+    free(matrix.val[i]);
+  free(matrix.val);
 }
 
 void print_matrix(Matrix matrix, int is_directed) {
-    for (int i = 0; i < matrix.n; i++) {
-        for (int j = 0; j < matrix.n; j++) {
-            if (is_directed)
-                printf("%d ", matrix.val[i][j]);
-            else
-                printf("%d ", matrix.val[i][j] | matrix.val[j][i]);
-        }
-        printf("\n");
+  for (int i = 0; i < matrix.n; i++) {
+    for (int j = 0; j < matrix.n; j++) {
+      if (is_directed)
+        printf("%d ", matrix.val[i][j]);
+      else
+        printf("%d ", matrix.val[i][j] | matrix.val[j][i]);
     }
-
     printf("\n");
+  }
+
+  printf("\n");
 }
