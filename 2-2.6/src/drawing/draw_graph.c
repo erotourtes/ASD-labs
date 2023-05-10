8446372d04b92eaa98201afbab38c9b229f65ac0
Max Sir <erotourtes@gmail.com>
2023-05-10 15:52:37 +0300

    feat: drawing weights

--- 2-2.6/src/drawing/draw_graph.c
+++ 2-2.6/src/drawing/draw_graph.c
@@ -2,9 +2,11 @@
 #include <math.h>
 #include <limits.h>
 #include <stdlib.h>
+#include <string.h>
 #include "../matrix/matrix.h"
 #include "draw_graph.h"
 #include "graph_coords.h"
+#include "../Utils/list.h"
 
 typedef unsigned long u64;
 
@@ -24,6 +26,21 @@ void draw_node(X11 app, Point point, int number, int circle_radius, u64 bg, u64 
                 (number < 9 ? 1 : 2));
 
 }
+
+void print_weight(X11 app, Point p, int number) {
+    char text[4];
+    sprintf(text, "%d", number);
+
+    // drawing square to better see the text
+    XSetBackground(app.dis, app.gc, 0xFFFFFF);
+    XSetForeground(app.dis, app.gc, 0xFFFFFF);
+    XFillRectangle(app.dis, app.win, app.gc, p.x, p.y - 10, 12, 12);
+
+    // drawing text
+    XSetForeground(app.dis, app.gc, 0xFF0000);
+    XDrawString(app.dis, app.win, app.gc, p.x, p.y, text, number == 0 ? 1 : (int) (log10(number) + 1));
+}
+
 
 void draw_nodes(X11 app, Point *points, int len, int circle_radius, unsigned long bg, unsigned long fg) {
     for (int i = 0; i < len; i++) {
@@ -79,7 +96,7 @@ void draw_head_arrow(X11 app, Point p, double deg) {
     XDrawLine(app.dis, app.win, app.gc, rx, ry, p.x, p.y);
 }
 
-void draw_loop(X11 app, double circle_radius, Point p, int is_directed) {
+void draw_loop(X11 app, double circle_radius, int weight, Point p, int is_directed) {
     int arc_width = floor(circle_radius * 1.5);
     int arc_height = floor(circle_radius * 1.5);
     XDrawArc(app.dis, app.win, app.gc, p.x - circle_radius - arc_width / 2.0, p.y - arc_height, arc_width, arc_height,
@@ -89,13 +106,19 @@ void draw_loop(X11 app, double circle_radius, Point p, int is_directed) {
     Point arrow_point = {p.x - circle_radius, p.y};
     if (is_directed)
         draw_head_arrow(app, arrow_point, -15);
+
+    print_weight(app, (Point){ p.x - arc_width / 2, p.y - arc_height / 2 }, weight);
 }
 
-void draw_line(X11 app, Point p1, Point p2) {
+void draw_line(X11 app, Point p1, Point p2, int weight) {
     XDrawLine(app.dis, app.win, app.gc, p1.x, p1.y, p2.x, p2.y);
+    if (weight != -1) {
+        Point p = (Point) {(p1.x + p2.x) / 2, (p1.y + p2.y) / 2};
+        print_weight(app, p, weight);
+    }
 }
 
-void draw_arrow_line(X11 app, Point p1, Point p2, double circle_radius) {
+void draw_arrow_line(X11 app, Point p1, Point p2, double circle_radius, int weight) {
     double dy = -(p2.y - p1.y);
     double dx = (p2.x - p1.x);
     double angle = atan(dy / dx);
@@ -109,10 +132,11 @@ void draw_arrow_line(X11 app, Point p1, Point p2, double circle_radius) {
         draw_head_arrow(app, p, angle * 180 / M_PI);
     }
 
-    draw_line(app, p1, p2);
+    return draw_line(app, p1, p2, weight);
 }
 
-void draw_through_angle(X11 app, Point *points, int i, int j, int circle_radius, int is_directed, int is_small) {
+void
+draw_through_angle(X11 app, Point *points, int i, int j, int weight, int circle_radius, int is_directed, int is_small) {
     Point p1 = points[i];
     Point p2 = points[j];
 
@@ -153,35 +177,39 @@ void draw_through_angle(X11 app, Point *points, int i, int j, int circle_radius,
 
     Point middle = (Point) {(p1.x + p2.x) / 2 - offsetX, (p1.y + p2.y) / 2 - offsetY};
 
-    draw_line(app, points[i], middle);
+    draw_line(app, points[i], middle, -1);
     if (is_directed)
-        draw_arrow_line(app, middle, points[j], circle_radius);
+        draw_arrow_line(app, middle, points[j], circle_radius, -1);
     else
-        draw_line(app, middle, points[j]);
+        draw_line(app, middle, points[j], -1);
+
+    print_weight(app, middle, weight);
 }
 
-void draw_right_line(X11 app, Matrix matrix, Point *points, int i, int j, int circle_radius, int is_directed) {
+void
+draw_right_line(X11 app, Matrix matrix, Point *points, int i, int j, int weight, int circle_radius, int is_directed) {
     if (i == j)
-        draw_loop(app, circle_radius, (Point) {points[i].x, points[i].y}, is_directed);
+        draw_loop(app, circle_radius, weight, (Point) {points[i].x, points[i].y}, is_directed);
     else if (is_through_center(points, i, j, matrix.n) || is_overlapping(points, matrix.n, i, j))
-        draw_through_angle(app, points, i, j, circle_radius, is_directed, 0);
+        draw_through_angle(app, points, i, j, weight, circle_radius, is_directed, 0);
     else if (is_directed && is_arrows_overlaps(matrix, i, j))
-        draw_through_angle(app, points, i, j, circle_radius, is_directed, 1);
+        draw_through_angle(app, points, i, j, weight, circle_radius, is_directed, 1);
     else {
         if (is_directed)
-            draw_arrow_line(app, points[i], points[j], circle_radius);
+            draw_arrow_line(app, points[i], points[j], circle_radius, weight);
         else
-            draw_line(app, points[i], points[j]);
+            draw_line(app, points[i], points[j], weight);
     }
 }
 
-void draw_graph(X11 app, Matrix matrix, Point *points, int is_directed, int circle_radius) {
+void draw_graph(X11 app, Matrix matrix, Matrix weights, Point *points, int is_directed, int circle_radius) {
     XSetLineAttributes(app.dis, app.gc, 2, LineSolid, CapButt, JoinMiter);
 
     for (int i = 0; i < matrix.n; i++) {
         for (int j = 0; j < matrix.n; j++) {
             if (matrix.val[i][j] == 0) continue;
-            draw_right_line(app, matrix, points, i, j, circle_radius, is_directed);
+            int weight = weights.val[i][j];
+            draw_right_line(app, matrix, points, i, j, weight, circle_radius, is_directed);
             XSetForeground(app.dis, app.gc, rand() % USHRT_MAX);
         }
     }
@@ -209,7 +237,7 @@ void highlight_edge_between(X11 app, Matrix m, Point *points, int from_node, int
         draw_node(app, from_point, from_node + 1, circle_radius, visited_color, fg);
     } else {
         XSetForeground(app.dis, app.gc, arrow_color);
-        draw_right_line(app, m, points, from_node, to_node, circle_radius, 1);
+        draw_right_line(app, m, points, from_node, to_node, 0, circle_radius, 1);
         draw_node(app, from_point, from_node + 1, circle_radius, current_color, fg);
         draw_node(app, to_point, to_node + 1, circle_radius, next_color, fg);
     }
--- 2-2.6/src/drawing/draw_graph.h
+++ 2-2.6/src/drawing/draw_graph.h
@@ -9,7 +9,7 @@
 #ifndef INC_2_2_5_DRAW_GRAPH_H
 #define INC_2_2_5_DRAW_GRAPH_H
 
-void draw_graph(X11 app, Matrix matrix, Point *points, int is_directed, int circle_radius);
+void draw_graph(X11 app, Matrix matrix, Matrix weights, Point *points, int is_directed, int circle_radius);
 
 void highlight_edge_between(X11 app, Matrix matrix, Point* coordinates, int from_node, int to_node, int circle_radius);
 
